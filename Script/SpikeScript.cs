e539faea23924688aa85c40159228da6ab47d589
Raj Pathare <rajgpathare@gmail.com>
2019-02-18 20:25:25 +0530

    Add files via upload

---
+++ Script/SpikeScript.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpikeScript : MonoBehaviour {
+
+
+	public GameObject leftwall1;
+	public GameObject leftwall2;
+	public GameObject leftwall3;
+	public GameObject leftwall4;
+	public GameObject leftwall5;
+	public GameObject leftwall6;
+
+	public GameObject rightwall1;
+	public GameObject rightwall2;
+	public GameObject rightwall3;
+	public GameObject rightwall4;
+	public GameObject rightwall5;
+
+	
+
+
+
+
+	int count = 0;
+	int score = 0;
+	public Text Score;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Score.text = score.ToString();
+		
+	}
+
+
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+
+		if(other.gameObject.tag == "Lspike" || other.gameObject.tag == "Rspike")
+		{
+
+			Destroy(this.gameObject);
+
+		}
+
+		if(other.gameObject.tag == "rightWall" && count == 0)
+		{
+
+			
+			leftwall1.SetActive(true);
+			//rightWall1.SetActive(false);
+			count++;
+			score++;
+
+
+		}
+
+		if(other.gameObject.tag == "leftWall" && count == 1)
+		{
+			leftwall6.SetActive(false);
+			leftwall1.SetActive(false);
+			rightwall1.SetActive(true);
+			count++;
+			score++;
+
+		}
+
+		if(other.gameObject.tag == "rightWall" && count == 2)
+		{
+
+			
+			leftwall2.SetActive(true);
+			rightwall1.SetActive(false);
+			count++;
+			score++;
+
+
+		}
+
+		if(other.gameObject.tag == "leftWall" && count == 3)
+		{
+			
+			leftwall2.SetActive(false);
+			rightwall2.SetActive(true);
+			count++;
+			score++;
+
+		}
+
+		if(other.gameObject.tag == "rightWall" && count == 4)
+		{
+
+			
+			leftwall3.SetActive(true);
+			rightwall2.SetActive(false);
+			count++;
+			score++;
+
+
+		}
+
+		if(other.gameObject.tag == "leftWall" && count == 5)
+		{
+			
+			leftwall3.SetActive(false);
+			rightwall3.SetActive(true);
+			count++;
+			score++;
+
+		}
+
+		if(other.gameObject.tag == "rightWall" && count == 6)
+		{
+
+			
+			leftwall4.SetActive(true);
+			rightwall3.SetActive(false);
+			count++;
+			score++;
+
+
+		}
+
+		if(other.gameObject.tag == "leftWall" && count == 7)
+		{
+			
+			leftwall4.SetActive(false);
+			rightwall4.SetActive(true);
+			count++;
+			score++;
+
+
+		}
+
+		if(other.gameObject.tag == "rightWall" && count == 8)
+		{
+
+			
+			leftwall5.SetActive(true);
+			rightwall4.SetActive(false);
+			count++;
+			score++;
+
+
+
+		}
+
+		if(other.gameObject.tag == "leftWall" && count == 9)
+		{
+			
+			leftwall5.SetActive(false);
+			rightwall5.SetActive(true);
+			count++;
+			score++;
+
+		}
+
+		if(other.gameObject.tag == "rightWall" && count == 10)
+		{
+
+			
+			leftwall6.SetActive(true);
+			rightwall5.SetActive(false);
+			score++;
+			count = 1;
+
+		}
+
+
+
+
+	}
+
+
+}
---
+++ Script/playerMovement.cs
@@ -0,0 +1,111 @@
+﻿    using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerMovement : objMovement
+{
+
+    [SerializeField] private float jumpPower = 2.288f;
+    [SerializeField] private float gravityVal = -0.3f;
+    [SerializeField] private float jumpAirPower = 0.5f;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] public static bool isGameOver;
+
+   
+
+
+    void Start()
+    {
+        
+        isGameOver = false;
+        Physics2D.gravity = new Vector2(Physics2D.gravity.x, gravityVal);
+        rb = GetComponent<Rigidbody2D>();
+    
+        speed = 3.5f;
+        direction = 1;
+       
+        
+        Destroy(GetComponent<PolygonCollider2D>());
+        gameObject.AddComponent<PolygonCollider2D>();
+
+
+
+        rb.velocity = new Vector2(speed * direction, rb.velocity.y);
+
+
+    }
+
+
+   /* private void Update()
+    {
+        DirectionChange();
+        hmm();
+        
+
+    }*/
+
+     private void Update()
+    {
+        DirectionChange();
+        /*if ((Input.GetKeyDown("space") || Input.touchCount > 0) && !isGameOver)
+        {
+            Jump();
+
+        }*/
+
+         if (Input.GetKeyDown("space") && !isGameOver)
+        {
+            Jump();
+
+        }
+        
+
+        //EXPERIMENTAL
+       /* if (rb.velocity.y < 0f)
+		{
+			rb.AddForce(new Vector2(0, -0.4f), ForceMode2D.Impulse);
+		}*/
+        //Debug.Log(rb.velocity);
+
+    }
+
+
+   /* public void hmm(){
+
+
+    	if ((Input.GetKeyDown("space") || Input.touchCount == 1) && !isGameOver)
+        {
+           
+            rb.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
+
+        }
+    }*/
+
+      private void Jump()
+    {
+        Debug.Log("hmm");
+            rb.AddForce(new Vector2(0, jumpPower) , ForceMode2D.Impulse);
+            
+        
+
+        /*if (rb.velocity.y >= 2f && rb.velocity.y <= 4f)
+        {
+            rb.AddForce(new Vector2(0, jumpAirPower), ForceMode2D.Impulse);
+        }*/
+
+
+    }
+
+    public void andrjump(){
+
+        rb.AddForce(new Vector2(0, jumpPower) , ForceMode2D.Impulse);
+
+    }
+
+   
+
+
+}
+
