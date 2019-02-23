6284908899ab8d436341fd5e3cb81f597a4b4ac1
Raj Pathare <rajgpathare@gmail.com>
2019-02-23 22:47:14 +0530

    Add files via upload

---
+++ Script/SpikeMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeMove : MonoBehaviour {
+
+	
+	public Vector3 targetPosition;
+    public float smoothFactor = 2;
+    public Vector3 targetPosition2;
+	
+
+
+	// Use this for initialization
+	void Start () {
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		var pos = new Vector3(0f,0f,0f);
+
+		
+		if(this.gameObject.activeInHierarchy){
+
+		transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smoothFactor);
+
+		Invoke("goback",2);
+
+		}
+		
+
+	
+	}
+
+	public void goback(){
+
+		transform.position = Vector3.Lerp(transform.position, targetPosition2, Time.deltaTime * smoothFactor);
+		
+
+	}
+
+
+	
+}
